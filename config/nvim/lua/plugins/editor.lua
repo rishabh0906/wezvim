c1ba88bff43bd4035e230ef3b0b4f5babe1af3bd
Rishabh Jain <rishabhjain@Rishabhs-MacBook-Air.local>
2026-07-05 17:20:40 +0530

    added nvim-tree and removed oil for explorer

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -1,10 +1,35 @@
 return {
   {
-    "stevearc/oil.nvim",
+    "nvim-tree/nvim-tree.lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("oil").setup()
-      vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
+      require("nvim-tree").setup({
+        view = {
+          width = 32,
+        },
+        renderer = {
+          group_empty = true,
+          highlight_git = true,
+          icons = {
+            show = {
+              git = true,
+              folder = true,
+              file = true,
+              folder_arrow = true,
+            },
+          },
+        },
+        filters = {
+          dotfiles = false,
+        },
+        git = {
+          enable = true,
+          ignore = false,
+        },
+      })
+
+      vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle file tree" })
+      vim.keymap.set("n", "<leader>E", "<cmd>NvimTreeFindFile<cr>", { desc = "Reveal current file" })
     end,
   },
 
